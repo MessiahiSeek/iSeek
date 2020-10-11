44a72f0b81451eeb637a14f862a4bf6a6ac56135
jk1551 <joe.k659@gmail.com>
2020-10-11 16:53:21 -0400

    new home screen

--- iCamera.js
+++ iCamera.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Component  } from 'react';
-import { Text, View, TouchableOpacity, ref, StyleSheet, ActivityIndicator,Image  } from 'react-native';
-import {  Button, ButtonGroup, Icon, Layout, Spinner } from '@ui-kitten/components';
+import { Text, View, TouchableOpacity, ref, StyleSheet, ActivityIndicator,Image, ImageBackground  } from 'react-native';
+import {  Button, ButtonGroup,/* Icon*/ Layout, Spinner } from '@ui-kitten/components';
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import { Audio } from 'expo-av';
@@ -8,6 +8,14 @@ import * as Permissions from 'expo-permissions';
 import * as FileSystem from 'expo-file-system';
 import { render } from 'react-dom';
 import * as Speech from 'expo-speech';
+import Icon from 'react-native-vector-icons/Ionicons';
+import ActionButton from 'react-native-action-button';
+import { Left } from 'native-base';
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings([
+  'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
+])
 
 
 
@@ -44,6 +52,11 @@ export const XCamera =({navigation}) => {
   const [recording, setRecording] = useState(null);
   //const useRef = useRef(null);
   const [photoJson,setPhoto] = useState("");
+  const [objectsInPic,SetObjectsInPhoto] = useState("");
+  const [isPictureFetching, setIsPictureFetching] = useState(false);
+  const [picStr,setPicStr] = useState("");
+  const [textInPic,setTextinPic] = useState("");
+  const [Load,SetLoad] = useState(false);
 
 
 
@@ -57,14 +70,12 @@ export const XCamera =({navigation}) => {
 
   snap = async () => {
     if (this.camera) {
+      setIsPictureFetching(true);
       const options = { quality: 1, base64: true, fixOrientation: true, 
         exif: true};
         await this.camera.takePictureAsync(options).then(photo => {
            photo.exif.Orientation = 1;            
-           //console.log(photo.base64); 
-           //getTime();  
-           //MediaLibrary.saveToLibraryAsync(photo.uri); 
-
+            setPicStr(photo.base64);
            fetch('http://ec2-3-23-33-73.us-east-2.compute.amazonaws.com:5000/image',
            {
              method: 'POST',
@@ -78,28 +89,16 @@ export const XCamera =({navigation}) => {
            }).then((response) => response.json())
            .then((json) => {
              setPhoto(json.pictureResponse);
-             console.log(photoJson);
-             //console.log("hello world")
+             SetObjectsInPhoto(json.objects);
+             //console.log(photoJson);
+             //console.log("hello world")npm
+             setIsPictureFetching(false);
             })
          });
          
      }  
+     
    }
- /*
- getTime =  () =>{ 
-   console.log("hello");
-   let response =  fetch('http://ec2-3-23-33-73.us-east-2.compute.amazonaws.com:5000/time').then((resp)=>{
-     console.log("hello agagin")
-     return resp.json()}).then(
-       (
-         (json)=>{
-     console.log(json);
-     const tim = json.time;
-     //tim = tim.toString();
-     console.log(tim);
-   }));
-   
- };*/
 
   
   startRecording = async () => {
@@ -183,17 +182,91 @@ const handleOnPressOut = () => {
     return <Text>No access to camera</Text>;
   }
   
+  SavePicture = async () =>{
+    const filename = FileSystem.documentDirectory + "iSeekPhoto.jpg";
+    await FileSystem.writeAsStringAsync(filename, photoJson, {
+    encoding: FileSystem.EncodingType.Base64,
+    });
+    await MediaLibrary.saveToLibraryAsync(filename);
+  }
+  ListObjects = async () => {
+    let obj = objectsInPic.split('\n');
+    Speech.speak("The Objects in this picture are:");
+    for(i = 0; i < obj.length; i++){
+      if (obj[i+1] === "" ){
+        Speech.speak("and " +obj[i]);
+        return;
+      }
+      else{
+      Speech.speak(obj[i]);
+    }
+  }
+  }
+  findText = async () => {
+    SetLoad(true);
+     fetch('http://ec2-3-23-33-73.us-east-2.compute.amazonaws.com:5000/text',
+           {
+             method: 'POST',
+             headers:{
+               Accept: 'application/json',
+               'Content-Type': 'application/json',
+             },
+             body: JSON.stringify({
+               pictureString: picStr,
+             }),
+           }).then((response) => response.json())
+           .then((json) => {
+            console.log(json.imageText);
+             setTextinPic(json.imageText);
+             SetLoad(false);
+
+
+           if (textInPic === null){
+            Speech.speak("There is no  text in this picture.");
+           }
+           else{
+           Speech.speak("The text in this picture is ");
+           Speech.speak(json.imageText);
+           }
+          }
+           )
+           
+  }
   return (
     
     <View style={styles.container}>
-      
-      <Camera style={{ flex: 1 }} type={type} ref={ref => { this.camera = ref; }}>
+        {(photoJson != "" && !isPictureFetching)  && (
+          <ImageBackground source ={{ uri:`data:image/jpg;base64,${photoJson}`}} style={{flex:1, height: undefined, width: undefined}}>
+            {(Load) && (<ActivityIndicator alignContent="center" size="large" color="#000" 
+            style={{position:"absolute"}}> </ActivityIndicator>)}
+          <View style={styles.close}>
+          <Button title="Save Picture" style={{position:"absolute", backgroundColor:'#F50303',borderRadius:10,borderWidth: 1,borderColor: '#fff'}} onPress={async () => this.SavePicture()}> Save Picture</Button>
+          </View>
+          <ActionButton style={styles.close2} buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#f0fff1' title="Read Objects out loud" onPress={()=>this.ListObjects()}>
+            <Icon name="ios-text"   onPress={()=>this.ListObjects()}/>
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#5f6702' title="Find text in screen"onPress={()=>this.findText()} >
+            <Icon name="ios-book" onPress={()=>this.findText()}/>
+          </ActionButton.Item>
+          </ActionButton>
+          
+          </ImageBackground>
+          )}
+
+
+          
+
+      {(isPictureFetching)&&<View style={[styles.container_nik,styles.horizontal]}>
+        <ActivityIndicator alignContent="center" size="large" color="#000"></ActivityIndicator>
+      </View>}
+
+
+      {(photoJson == "" && !isPictureFetching) &&(
+        <Camera style={{ flex: 1 }} type={type} ref={ref => { this.camera = ref; }}>
         <View>
-        {(photoJson != "")  && (<Image source ={{ uri:`data:image/gif;base64,${photoJson}`}} 
-      style={{alignContent:'center',justifyContent:'center',width:400,height:600}}/>)}
           <ButtonGroup style={styles.buttongroup} size='large'>
-        <Button onPress={ async () =>  this.snap()}>
-          Camera</Button>
+        <Button onPress={ async () =>  this.snap()}> Camera</Button>
           <Button onPress={() => {
               setType(
                 type === Camera.Constants.Type.back
@@ -202,17 +275,15 @@ const handleOnPressOut = () => {
               );
             }}>Flip</Button>
         
-        <Button
-                    onPressIn={handleOnPressIn}
-                    onPressOut={handleOnPressOut}
-                >
+        <Button onPressIn={handleOnPressIn}    onPressOut={handleOnPressOut}>
                     {isFetching && <ActivityIndicator/>}
                     {!isFetching && <Text>Voice Search</Text>}
                 </Button>
                 </ButtonGroup>
-                
-        </View>
-      </Camera>
+                </View>
+                </Camera>
+      ) }
+        
     </View>
    ); 
   }
@@ -220,11 +291,44 @@ const handleOnPressOut = () => {
         
       
 const styles = StyleSheet.create({
+  container_nik: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
 
   container: {
     flex: 1,
   },
-  
+  close: {
+    position: 'absolute',
+    width: '100%', 
+    padding: 30,
+    justifyContent: 'center', 
+    alignItems: 'flex-start',
+   bottom: 45,
+   marginLeft:'5%',
+
+  },
+  close2: {
+    position: 'absolute',
+    width: '100%', 
+    padding: 30,
+    justifyContent: 'center', 
+    alignItems: 'flex-start',
+   bottom: 20,
+   marginRight:'5%',
+
+  },
   buttongroup: {
     flexDirection: 'row',
     alignItems: 'center',
