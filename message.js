b46fd8390c4b25ecb360433ae03a1d90af012536
lukemeads3 <lameads3@gmail.com>
2020-11-23 18:07:24 -0500

    message page

--- message.js
+++ message.js
@@ -1,40 +1,74 @@
-import { Container, View } from 'native-base';
 import React, { useState } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, SystemMessage } from 'react-native-gifted-chat';
+import { XCamera } from './iCamera.js';
+import { settingspage } from './settingspage.js';
+import { faqpage } from './faq.js';
 
 export const message =({navigation}) => {
+  
 
   const [messages, setMessages] = useState([
     {
       _id: 0,
-      text: 'New chat started.',
-      createdAt: new Date().getTime(),
-      system: true
-    },
-    {
-      _id: 1,
-      text: 'Hello!',
+      text: 'Hello! How may I assist you?',
       createdAt: new Date().getTime(),
       user: {
         _id: 2,
-        name: 'Test User'
+        name: 'System',
+        avatar: require('./assets/splash.png')
       }
+    },
+    {
+      _id: 1,
+      text: 'New chat started.',
+      createdAt: new Date().getTime(),
+      system: true
     }
+    
   ]);
 
+
   function handleSend(newMessage = []){
-    setMessages(GiftedChat.append(messages, newMessage));
-  }
 
+    setMessages(prevState => [...prevState, newMessage]);
 
-  
+    var userText = newMessage[0].text;
+    console.log(userText);
+
+    fetch('http://iseek.cs.messiah.edu:5000/chatbot',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        textString: userText,
+      })
+  })
+  .then((response) => response.json())
+  .then((json) => {
+    setMessages(prevState => [...prevState, {
+      _id: 3,
+      text: json,
+      createdAt: new Date().getTime(),
+      system: true
+    }])
+  })
+ 
+}
+
+function voiceNav (nav){
+switch(nav){
+  case 1:
+}
+}
+
   return(
-
     <GiftedChat
-    messages={messages}
     onSend={newMessage => handleSend(newMessage)}
-    user={{ _id: 1 }}
+    messages={messages}
+    user={{ _id: 1, name: 'Luke' }}
+    placeholder="Ask your quesiton here..."
+    showUserAvatar
+    alwaysShowSend
+    scrollToBottom
     />
-    
   );
 }
