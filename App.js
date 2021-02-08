9634dbfadbf1f8d7d0a5d5ebe4abdc81d89bd5e4
lukemeads3 <lameads3@gmail.com>
2021-02-08 16:57:07 -0500

    updated work on darkmode

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, Component }  from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout } from '@ui-kitten/components';
 import { DarkTheme, DefaultTheme, getFocusedRouteNameFromRoute, NavigationContainer, useIsFocused, useTheme } from '@react-navigation/native';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem, } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem} from '@react-navigation/drawer';
 import { XCamera } from './iCamera.js';
 import { streamingPage } from './streaming.js';
 import { settingspage } from './settingspage.js';
@@ -16,85 +16,58 @@ import { Switch, List, ListItem } from '@shoutem/ui';
 import Sidebar from './customDrawer.js';
 import { PropsService } from '@ui-kitten/components/devsupport';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { color } from 'react-native-reanimated';
 
 
 const Drawer = createDrawerNavigator();
-function HomeScreen({navigation}) {
+const AppDrawer = () => {
   const {colors} = useTheme();
   return (
-    <Container style={{Color: colors.card}}>
-      <Header>
-        <Left style={{flex: 0.1}} />
-        <Body style={{flex: 1, alignItems: 'center'}}>
-          <Title>Home</Title>
-        </Body>
-        <Right style={{flex: 0.1}} />
-      </Header>
-      <Content
-        contentContainerStyle={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-          <Text>HomeScreen</Text>
-          </Content>
-    </Container>
-  );
-}
-const MyTheme = {
-  dark: false,
-  colors: {
-    primary: 'rgb(255, 45, 85)',
-    background: 'rgb(242, 242, 242)',
-    card: 'rgb(255, 255, 255)',
-    text: 'rgb(28, 28, 30)',
-    border: 'rgb(199, 199, 204)',
-    notification: 'rgb(255, 69, 58)',
-  },
-};
-
-const AppDrawer = () => {
-  return (
-    <Drawer.Navigator  drawerContent={props => <Sidebar {...props} />}>
+    <Drawer.Navigator 
+    drawerContentOptions={{
+      inactiveTintColor: colors.text
+    }}
+    drawerContent={props => <Sidebar {...props} />}>
+      
       
       <Drawer.Screen
+    
       name="Camera"
       component={XCamera}
       options={{
         drawerIcon: ({focused, color, size}) => (
-          <Icon name="ios-camera" style={{fontSize: size, color: Colors.text}} />
+          <Icon name="ios-camera" style={{fontSize: size, color: colors.text}} />
         ),
       }}
       />
 
       <Drawer.Screen
-      name="Messenger"
-      color = {Colors.Text}
+      name="Messenger" 
       component={message}
       options={{
         drawerIcon: ({focused, color, size}) => (
-          <Icon name="ios-chatbubbles" style={{fontSize: size, color: color}} />
+          <Icon name="ios-chatbubbles" style={{fontSize: size, color: colors.text}} />
         ),
-      }}
+      }} 
       />  
 
       <Drawer.Screen
       name="FAQ"
-      color = {Colors.Title}
+      color = {colors.text}
       component={faqpage}
       options={{
         drawerIcon: ({focused, color, size}) => (
-          <Icon name="ios-book" style={{fontSize: size, color: color}} />
+          <Icon name="ios-book" style={{fontSize: size, color: colors.text}} />
         ),
       }}
       />
       <Drawer.Screen
       name="BETA Streaming"
-      color = {Colors.Title}
+      color = {colors.text}
       component={streamingPage}
       options={{
         drawerIcon: ({focused, color, size}) => (
-          <Icon name="ios-videocam" style={{fontSize: size, color: color}} />
+          <Icon name="ios-videocam" style={{fontSize: size, color: colors.text}} />
         ),
       }}
       />
@@ -111,6 +84,7 @@ function App() {
     let eventListener = EventRegister.addEventListener(
       'appChangeThemeEvent',
       data => {
+
         setDarkApp(data);
       },
     );
--- customDrawer.js
+++ customDrawer.js
@@ -4,15 +4,15 @@ import {
     DrawerItemList,
     DrawerItem,
     DrawerContent,
-    navigation,
-    DrawerActions,
+    navigation
 } from '@react-navigation/drawer';
-import { NavigationContainer, DefaultTheme, DarkTheme, useTheme} from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme, useTheme, DrawerActions} from '@react-navigation/native';
 import {Icon, Header, Content, Footer, List, ListItem, Body, Text, Right, Left, Switch, H3, colors, Thumbnail, Button} from 'native-base';
 import Animated, { color } from 'react-native-reanimated';
 import { Container } from 'semantic-ui-react';
 import { View } from '@shoutem/ui';
 import { EventRegister } from 'react-native-event-listeners';
+
 
 // Credit for this code goes to @ReactNativeTutorials
 
@@ -27,15 +27,16 @@ function Sidebar({progress, ...props}) {
         <NavigationContainer independent={true}>
             <Header style={{backgroundColor: colors.card, borderBottomWidth: 0}}>
                 <Right>
-                    <Button transparent>
+                    <Button transparent onPress={()=>
+                         props.navigation.dispatch(DrawerActions.closeDrawer())} >
                         <Icon style=
                         {{color: colors.text}}
                         name="menu" />
                     </Button>
                 </Right>
             </Header>
-            <Content contentContainerStyle={{flex: 1, backgroundColor:color.card}}>
-                <ListItem thumbnail>
+            <Content style={{color: colors.text}} contentContainerStyle={{flex: 1, backgroundColor:color.card}}>
+                <ListItem thumbnail>  
                     <Left>
                         <Thumbnail
                         source={require("./assets/icon.png")} />
@@ -44,11 +45,13 @@ function Sidebar({progress, ...props}) {
                         <H3 style={{color: colors.text}}>ISeek</H3>
                     </Body>
                 </ListItem>
-        <DrawerContentScrollView {...props}>
+        <DrawerContentScrollView {...props} >
             <Animated.View style={{transform: [{translateX}]}}>
-            <DrawerItemList {...props} />
+            <DrawerItemList {...props}/>
         </Animated.View>
         </DrawerContentScrollView>
+        <List>
+        </List>
                 <List>
                     <ListItem>
                         <Body>
